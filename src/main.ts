c3a68c4b6af3abf9ed03d24ee94ba2a0797f07bf
vonhatthanh2000 <nthanhatee@gmail.com>
2024-07-03 10:26:09 +0700

    chore: add port to main ts

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerOption);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3035);
 }
 bootstrap();
