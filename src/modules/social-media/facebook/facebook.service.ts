c5ce18944cbc6bb364b0cf2b9c0c1d773296fde8
nhatthanh.vo <nthanhatee@gmail.com>
2024-07-19 14:30:02 +0700

    fix: fb service

--- src/modules/social-media/facebook/facebook.service.ts
+++ src/modules/social-media/facebook/facebook.service.ts
@@ -1,8 +1,70 @@
 import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
 import { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+
+@Injectable()
 export class FacebookService {
   constructor(private readonly httpService: HttpService) {}
+
+  async hello() {
+    return 'Hello World!';
+  }
+
+  async handleGetWebhook(req: Request, res: Response) {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Check the mode and token sent is correct
+
+      if (mode === 'subscribe' && token === FB_VERIFY_TOKEN) {
+        // Respond with the challenge token from the request
+        res.status(200).send(challenge);
+      } else {
+        // Respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+  }
+
+  async handleSendWebhook(req: Request, res: Response) {
+    let body = req.body;
+
+    // Checks if this is an event from a page subscription
+    if (body.object === 'page') {
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(async (entry) => {
+        // Gets the body of the webhook event
+        let webhookEvent = entry.messaging[0];
+
+        // Get the sender PSID
+        let senderPsid = webhookEvent.sender.id;
+        console.log('Sender PSID: ' + senderPsid);
+
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhookEvent.message) {
+          this.handleMessage(senderPsid, webhookEvent.message, req);
+        } else if (webhookEvent.postback) {
+          this.handlePostback(senderPsid, webhookEvent.postback, req);
+        }
+      });
+
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  }
 
   async handleMessage(senderPsid, receivedMessage, request: Request) {
     let response;
@@ -50,7 +112,7 @@ export class FacebookService {
     this.callSendAPI(senderPsid, request, response);
   }
 
-  async handlePostback(senderPsid, request: Request, receivedPostback) {
+  async handlePostback(senderPsid, receivedPostback, request: Request) {
     let response;
 
     // Get the payload for the postback
--- src/modules/social-media/social-media.controller.ts
+++ src/modules/social-media/social-media.controller.ts
@@ -14,59 +14,11 @@ export class SocialMediaController {
 
   @Get('facebook/webhooks')
   async getwebhook(@Req() req: Request, @Res() res: Response) {
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    console.log('req :>> ', req);
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-    // Check if a token and mode is in the query string of the request
-    if (mode && token) {
-      // Check the mode and token sent is correct
-
-      if (mode === 'subscribe' && token === FB_VERIFY_TOKEN) {
-        // Respond with the challenge token from the request
-        res.status(200).send(challenge);
-      } else {
-        // Respond with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);
-      }
-    }
+    return this.fbService.handleGetWebhook(req, res);
   }
 
   @Post('facebook/webhooks')
   async postwebhook(@Req() req: Request, @Res() res: Response) {
-    let body = req.body;
-
-    console.log('body :>> ', body);
-
-    // Checks if this is an event from a page subscription
-    if (body.object === 'page') {
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function (entry) {
-        // Gets the body of the webhook event
-        let webhookEvent = entry.messaging[0];
-        console.log(webhookEvent);
-
-        // Get the sender PSID
-        let senderPsid = webhookEvent.sender.id;
-        console.log('Sender PSID: ' + senderPsid);
-
-        // Check if the event is a message or postback and
-        // pass the event to the appropriate handler function
-        if (webhookEvent.message) {
-          this.fbService.handleMessage(senderPsid, webhookEvent.message);
-        } else if (webhookEvent.postback) {
-          this.fbService.handlePostback(senderPsid, webhookEvent.postback);
-        }
-      });
-
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
+    return this.fbService.handleSendWebhook(req, res);
   }
 }
