2db4aca90c20df08f85ec11b25eb272d7b89005c
nhatthanh.vo <nthanhatee@gmail.com>
2024-08-02 13:45:14 +0700

    fix: discord config

--- src/config/discord.config.ts
+++ src/config/discord.config.ts
@@ -1,7 +1,7 @@
 import { HttpModuleOptions } from '@nestjs/axios';
 import { registerAs } from '@nestjs/config';
 
-export default registerAs<HttpModuleOptions>('ai-service', () => ({
+export default registerAs<HttpModuleOptions>('discord-service', () => ({
   baseURL: process.env.DISCORD_URL,
   headers: {
     'Content-Type': 'application/json',
